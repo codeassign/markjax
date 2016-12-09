ee8e521adf0975abbbd619f2dd7088a7d84e6823
Herman Zvonimir Došilović <hermanz.dosilovic@gmail.com>
2016-12-09 02:15:05 +0100

    Load MathJax config dynamically

--- js/markjax.js
+++ js/markjax.js
@@ -1,29 +1,29 @@
-(function () {
-    var script = document.createElement("script");
-    script.type = "text/javascript";
-    script.src  = "http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML";
-    document.getElementsByTagName("head")[0].appendChild(script);
-})();
+var head = document.getElementsByTagName("head")[0], script;
+script = document.createElement("script");
+script.type = "text/x-mathjax-config";
+script[(window.opera ? "innerHTML" : "text")] =
+  "MathJax.Hub.Config({" +
+  "  showProcessingMessages: false," +
+  "  messageStyle: \"none\"," +
+  "  tex2jax: {" +
+  "    inlineMath: [['$','$']]," +
+  "    displayMath: [['$$', '$$']]," +
+  "    ignoreClass: \".*\"," +
+  "    processClass: \"mathjax\"" +
+  "  }," +
+  "  TeX: {" +
+  "    equationNumbers: {" +
+  "      autoNumber: \"AMS\"" +
+  "    }" +
+  "  }" +
+  "});";
+head.appendChild(script);
+script = document.createElement("script");
+script.type = "text/javascript";
+script.src  = "http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML";
+head.appendChild(script);
 
 var marked = require("marked");
-require("mathjax");
-
-MathJax.Hub.Config({
-  showProcessingMessages: false,
-  messageStyle: "none",
-  skipStartupTypeset: true,
-  tex2jax: {
-    inlineMath: [['$','$']],
-    displayMath: [['$$', '$$']],
-    ignoreClass: ".*",
-    processClass: "mathjax"
-  },
-  TeX: {
-    equationNumbers: {
-      autoNumber: "AMS"
-    }
-  }
-});
 
 marked.setOptions({
   breaks: true,
