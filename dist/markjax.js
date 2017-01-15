28d116b0dc6d7a39a51813d8c4b97dbcaf617aea
Herman Zvonimir Došilović <hermanz.dosilovic@gmail.com>
2017-01-15 17:21:04 +0100

    Load MathJax over HTTPS

--- dist/markjax.js
+++ dist/markjax.js
@@ -1,5 +1,5 @@
 /**
- * markjax v1.1.0
+ * markjax v1.1.1
  * Copyright CodeAssign
  * @link http://markjax.codeassign.com/
  * @license Apache-2.0
@@ -31,7 +31,7 @@
   
   var mathjax = document.createElement("script");
   mathjax.type = "text/javascript";
-  mathjax.src = "http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML";
+  mathjax.src = "https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML";
   head.appendChild(mathjax);
 
   var katex = document.createElement("link");
